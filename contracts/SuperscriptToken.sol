f9622236be16861e867e5d14a1b68bc02dec6d45
Luke Curtis <lukesimoncurtis@gmail.com>
2022-02-08 17:01:38 +0000

    Remove unused import

--- contracts/SuperscriptToken.sol
+++ contracts/SuperscriptToken.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "hardhat/console.sol";
 
 contract TBD is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
